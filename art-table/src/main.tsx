89514df64dae657fd3c53e8653ddfaad7cc5509c
Uday Shaker <udayshekarnallamolu@gmail.com>
2026-02-22 01:25:56 +0530

    finalCODE

--- art-table/src/main.tsx
+++ art-table/src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-// ✅ PrimeReact styles (ORDER IMPORTANT)
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
